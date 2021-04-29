9d21dd3da31fd9ac051e03724fd5fa64653bd48c
Taqtile <taqtile@Mr-Incredible.local>
2021-04-29 12:01:09 -0300

    Add Apollo Server setup

---
+++ src/apollo-server.js
@@ -0,0 +1,21 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return 'Hello world!';
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen(4000).then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
